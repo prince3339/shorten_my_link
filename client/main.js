d775a149077b39b6c44bcfb4152df83995f46a82
Sabbir <sabbir.siddiquee@selise.ch>
2018-03-12 01:17:38 +0600

    link create component added

--- client/main.js
+++ client/main.js
@@ -3,10 +3,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import Header from './components/header';
+import LinkCreate from './components/link_create';
 
 const App = () => {
   return (
-    <Header />
+    <div>
+      <Header />
+      <LinkCreate />
+    </div>
   );
 };
 
---
+++ imports/collections/links.js
@@ -0,0 +1,4 @@
+import { Meteor } from 'meteor/meteor';
+import { Mongo } from 'meteor/mongo';
+
+export const Links = new Mongo.Collection('links');
