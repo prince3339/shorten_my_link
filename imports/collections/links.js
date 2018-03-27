f9735c3d231f5551974c153b1b84c0fca7550c9e
Sabbir <sabbir.siddiquee@selise.ch>
2018-03-27 10:12:27 +0600

    meteor method added

--- imports/collections/links.js
+++ imports/collections/links.js
@@ -1,4 +1,25 @@
 import { Meteor } from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo';
+import { check, Match } from 'meteor/check';
+
+import validUrl from 'valid-url';
+
+Meteor.methods({
+    'links.insert'(url) {
+        console.log("Trying to save", url);
+        //validUrl.isUri(url);
+        check(url, Match.Where(url => validUrl.isUri(url)));
+
+        //We are ready to save the URL
+        const token = Math.random().toString(36).slice(-5);
+        Links.insert(
+            {
+                url,
+                token,
+                clicks: 0
+            }
+        );
+    }
+});
 
 export const Links = new Mongo.Collection('links');
--- server/main.js
+++ server/main.js
@@ -1,5 +1,11 @@
 import { Meteor } from 'meteor/meteor';
+import { Links } from '../imports/collections/links';
 
 Meteor.startup(() => {
   // code to run on server at startup
+  if(Meteor.isServer) {
+    Meteor.publish('links', function() {
+      return Links.find({});
+    })
+  }
 });
