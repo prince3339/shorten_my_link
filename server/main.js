2b3edc50b7b89b5c82935f8c7c3d1bda32901f4e
Sabbir Ahmed Siddiquee <prince3339@ymail.com>
2018-04-22 19:37:02 +0600

    router middlewae added

--- server/main.js
+++ server/main.js
@@ -1,11 +1,19 @@
 import { Meteor } from 'meteor/meteor';
 import { Links } from '../imports/collections/links';
+import { WebApp } from 'meteor/webapp';
+import ConnectRoute from 'connect-route';
 
 Meteor.startup(() => {
   // code to run on server at startup
   if(Meteor.isServer) {
     Meteor.publish('links', function() {
       return Links.find({});
-    })
+    });
   }
 });
+
+const middleware = ConnectRoute(function(router) {
+  router.get('/:token', (req) => console.log(req));
+});
+
+WebApp.connectHandlers.use(middleware);
