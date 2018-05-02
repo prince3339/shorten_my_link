2c2cb73c186c36d37be6540efa35681c57d9010b
Sabbir <sabbir.siddiquee@selise.ch>
2018-05-02 22:16:39 +0600

    redirect and count increment implemented

--- server/main.js
+++ server/main.js
@@ -12,8 +12,29 @@ Meteor.startup(() => {
   }
 });
 
+//Executed whenever a user visits with a route like 
+// 'localhost:3000/abcd'
+function onRoute(req, res, next) {
+  //Take the token out of the url and try to find a 
+  //matching link in the Links collection
+  const link = Links.findOne( { token: req.params.token } );
+  
+  if(link) {
+    //If we find a link object, rirect the user to the long URL
+    Links.update(link, { $inc: {clicks: 1} });
+    link.clicks += 1;
+    res.writeHead(307, {
+      'Location': link.url
+    });
+    res.end();
+  } else {
+    //Orelse redirect to normal React app
+    next();
+  }
+}
+
 const middleware = ConnectRoute(function(router) {
-  router.get('/:token', (req) => console.log(req));
+  router.get('/:token', onRoute);
 });
 
 WebApp.connectHandlers.use(middleware);
